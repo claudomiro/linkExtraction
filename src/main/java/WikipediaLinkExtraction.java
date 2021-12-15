f39667ad5a6e14eabfb0ec9e81582b2fbeb8bc3b
Claudomiro Junior <claudomiro>
2021-12-15 09:16:56 -0300

    using Path to process input file

--- src/main/java/WikipediaLinkExtraction.java
+++ src/main/java/WikipediaLinkExtraction.java
@@ -1,12 +1,14 @@
 import lombok.extern.java.Log;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Log
 public class WikipediaLinkExtraction {
     public static void main(String[] args) {
-        log.info("Opening file '" + args[0] + "'");
-        File inputFile = new File(args[0]);
-        System.out.println(inputFile.exists());
+        Path inputPath = Paths.get(args[0]);
+        log.info("Opening path '" + inputPath + "'");
+        log.info("CAN READ:" + inputPath.toFile().canRead());
     }
 }
