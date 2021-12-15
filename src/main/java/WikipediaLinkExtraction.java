0d829d62dde9904c9c6ecee14a380e83c3d88814
Claudomiro Junior <claudomiro>
2021-12-15 09:06:11 -0300

    Using lombok to log

--- src/main/java/WikipediaLinkExtraction.java
+++ src/main/java/WikipediaLinkExtraction.java
@@ -1,2 +1,12 @@
+import lombok.extern.java.Log;
+
+import java.io.File;
+
+@Log
 public class WikipediaLinkExtraction {
+    public static void main(String[] args) {
+        log.info("Opening file '" + args[0] + "'");
+        File inputFile = new File(args[0]);
+        System.out.println(inputFile.exists());
+    }
 }
