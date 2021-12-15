c9eab7100b7746138786a8c750fbec75c6b92691
Claudomiro Junior <claudomiro>
2021-12-15 10:32:33 -0300

    Using Pattern object

--- src/main/java/WikipediaLinkExtraction.java
+++ src/main/java/WikipediaLinkExtraction.java
@@ -1,14 +1,42 @@
 import lombok.extern.java.Log;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Log
 public class WikipediaLinkExtraction {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get(args[0]);
         log.info("Opening path '" + inputPath + "'");
-        log.info("CAN READ:" + inputPath.toFile().canRead());
+
+        new WikipediaLinkExtraction(inputPath);
+    }
+    private static Pattern titlePattern = Pattern.compile(".*<title>(.+)</title>.*");
+
+    private String currentTitle = "";
+
+    public WikipediaLinkExtraction(Path inputPath) throws IOException {
+        run(inputPath);
+    }
+
+    private void run(Path inputPath) throws FileNotFoundException {
+        FileInputStream inputFIS = new FileInputStream(inputPath.toFile());
+        Scanner sc = new Scanner(inputFIS, "UTF-8");
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            Matcher titleMatcher = titlePattern.matcher(line);
+            if(titleMatcher.matches()) {
+                currentTitle = titleMatcher.group(1);
+                log.info("title line: '" + currentTitle + "'");
+            }
+
+        }
     }
 }
